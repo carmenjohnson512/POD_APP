0caebd52b740efb64cece7d25a7cc3a93222c1f4
Ariel Cuesta <arielcc88@gmail.com>
2020-08-24 23:22:43 -0500

    adding Heroku JAWSDB connection credentials for sequelize.

--- models/index.js
+++ models/index.js
@@ -9,20 +9,28 @@ const db = {};
 //DB connection with Sequelize
 //check if env var is set
 let sequelize;
-if (process.env.APP_ENV) {
-  //printing message if uding dev enviro nment
-  console.log(process.env.APP_ENV === "development" ? "USING DEV Vars for DB Connection\n" : "");
-  //instantiating DB connection with sequelize
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: process.env.SQL_DIALECT,
-    operatorsAliases: false //string based operators deprecated warning. (symbopl based ops will be used)
-  });
-} else {
-  //if not env set in .env, use config.json in pre-defined development env
-  const config = require(__dirname + '/../config/config.json')["development"];
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+//adding Heroku DB connection credentials
+
+  if (process.env.APP_ENV === "development") {
+    //printing message if uding dev enviro nment
+    console.log("USING DEV Vars for DB Connection\n");
+    //instantiating DB connection with sequelize
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+      host: process.env.DB_HOST,
+      dialect: process.env.SQL_DIALECT,
+      operatorsAliases: false //string based operators deprecated warning. (symbopl based ops will be used)
+    });
+  } else if ((process.env.APP_ENV === "deployment") && (process.env.JAWSDB_URL)) {
+    //adding Heroku credentials for JAWSDB connection
+    sequelize = new Sequelize(process.env.JAWSDB_URL, {
+      dialect: process.env.SQL_DIALECT,
+      operatorsAliases: false //string based operators deprecated warning. (symbol based ops will be used)
+    });
+  } else {
+    //if not env set in .env, use config.json in pre-defined development env
+    const config = require(__dirname + '/../config/config.json')["development"];
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }
 
 //identifying models from corresponding directory and loading them
 fs
